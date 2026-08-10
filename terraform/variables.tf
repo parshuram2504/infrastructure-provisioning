e36b21ad74385da7684858bc94a675ed43e647ff
parashuram2504@gmail.com <parshu@DESKTOP-14SED6P.localdomain>
2026-08-10 10:43:49 +0000

    AWS infra provisioning initial code

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
